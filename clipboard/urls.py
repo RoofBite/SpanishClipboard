62be926c51cdd5a813bd09a062ca0baa1bbf298e
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-06-29 17:48:14 +0200

    Basic registration added

--- clipboard/urls.py
+++ clipboard/urls.py
@@ -1,8 +1,9 @@
-
 from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('add_word/', views.add_word, name='add_word'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
--- clipboard/views.py
+++ clipboard/views.py
@@ -1,4 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate,login
 
 def add_word(request):
     return render(request,'clipboard/add_word.html')
+
+def register(request):
+    form=UserCreationForm()
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('add_word')
+    else:
+        return render(request,'clipboard/register.html',context={'form':form})
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('add_word')
+    if request.method=="POST":
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+    else:
+        return render(request,'clipboard/login.html')
+
+def logout(request):
+    return
