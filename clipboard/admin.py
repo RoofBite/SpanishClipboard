d97eccb7cde85743d40e1313220c55dce3c29739
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-06-30 11:23:18 +0200

    User model list_display in admin panel changed

--- clipboard/admin.py
+++ clipboard/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Word
+
 # Register your models here.
-admin.site.register(Word)
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display=('polish_word','spanish_word','etymology','date_added')
