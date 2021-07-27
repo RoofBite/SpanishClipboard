1e8961ce9d42b09af3dda1827253edb81712906b
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-07-27 23:52:28 +0200

    UserAccount models test full coverage

--- clipboard/tests/test_models.py
+++ clipboard/tests/test_models.py
@@ -1,26 +1,34 @@
 from django.test import TestCase
-from clipboard.models import Word, UserAccount
+from clipboard.models import Word, UserAccount, User
 
 class TestViews(TestCase):
 
-    
+    def setUp(self): 
+       self.user = User.objects.create_user('John', 'John@example.com', 'Password') 
+
+    # Word model tests
     def test_word_str_method_spanish_and_polish_word(self):
-        self.word=Word.objects.create(polish_word='polish_word', spanish_word = 'spanish_word')
+        self.word = Word.objects.create(polish_word = 'polish_word', spanish_word = 'spanish_word')
 
         self.assertEquals(str(Word.objects.first()), 'polish_word' + " - " + 'spanish_word')
     
     def test_word_str_method_only_spanish_word(self):
-        self.word=Word.objects.create(spanish_word = 'spanish_word')
+        self.word = Word.objects.create(spanish_word = 'spanish_word')
 
         self.assertEquals(str(Word.objects.first()), 'spanish_word')
 
     def test_word_str_method_only_polish_word(self):
-        self.word=Word.objects.create(polish_word='polish_word')
+        self.word = Word.objects.create(polish_word = 'polish_word')
 
         self.assertEquals(str(Word.objects.first()), 'polish_word')
 
     def test_word_str_method_no_word(self):
-        self.word=Word.objects.create(etymology='etymology')
+        self.word = Word.objects.create(etymology = 'etymology')
 
         self.assertEquals(str(Word.objects.first()), 'empty')
         
+    # UserAccount model tests
+    def test_useraccount_str_method(self):
+        self.useraccount = UserAccount.objects.create(user = self.user)
+
+        self.assertEquals(str(UserAccount.objects.first().user.username), 'John')
