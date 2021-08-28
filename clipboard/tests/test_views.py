c907fd7c495f8e6f5858cb18443008f1211c259c
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-08-28 19:37:25 +0200

    edit_word tests added

--- clipboard/tests/test_views.py
+++ clipboard/tests/test_views.py
@@ -469,3 +469,57 @@ class TestViews_retrive_word(TestCase):
 
         self.assertEquals(response.status_code, 302)
 
+
+
+
+class TestViews_edit_word(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user("John", "John@example.com", "Password")
+
+        self.word1 = Word.objects.create(
+            polish_word="polish_word",
+            spanish_word="spanish_word",
+            etymology="etymology",
+            notes="notes",
+            date_added="2021-01-01",
+            for_deletion=True,
+            user=self.user,
+        )
+
+    def test_edit_word_POST_authenticated(self):
+        client = Client()
+        client.login(username="John", password="Password")
+
+        response = client.post(reverse("edit_word", args=[1]))
+    
+        self.assertEquals(response.status_code, 302)
+    
+    def test_edit_word_POST_authenticated_redirect(self):
+        client = Client()
+        client.login(username="John", password="Password")
+
+        response = client.post(reverse("edit_word", args=[1]),{"redirect":"redirect"})
+    
+        self.assertEquals(response.status_code, 302)
+        
+    def test_edit_word_GET_authenticated(self):
+        client = Client()
+        client.login(username="John", password="Password")
+
+        response = client.get(reverse("edit_word", args=[1]))
+    
+        self.assertEquals(response.status_code, 200)
+    
+    def test_edit_word_GET_authenticated_no_word(self):
+        client = Client()
+        client.login(username="John", password="Password")
+        
+        response = client.get(reverse("edit_word", args=[1]))
+    
+        self.assertEquals(response.status_code, 200)
+
+    def test_edit_word_GET_not_authenticated(self):
+        client = Client()
+        response = client.get(reverse("edit_word",  args=[0]))
+
+        self.assertEquals(response.status_code, 302)
