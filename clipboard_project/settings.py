13da90ddea0174effcbfa48afe25628823d4e0c8
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-08-30 22:17:17 +0200

    Local postgersql datebase connected

--- clipboard_project/settings.py
+++ clipboard_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from .secrets import secret_key
 import os
+from .secrets import datebase_password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,8 +90,12 @@ WSGI_APPLICATION = "clipboard_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "demo_test",
+        "USER": "postgres",
+        "PASSWORD": datebase_password,
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
