ce021040be19d24c768e8e8a432a4fa1f3015d0b
Przemysław Maryńczak <marynczak.przemyslaw@gmail.com>
2021-10-06 20:10:48 +0200

    STATICFILES_DIRS commented out

--- clipboard_project/settings.py
+++ clipboard_project/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
